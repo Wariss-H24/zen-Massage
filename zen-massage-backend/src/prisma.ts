33d2411624c25e689bffb3254f377c94bf067a8c
Wariss Achamou Adeniyi BADA <wariss.bada@chem.loc>
2026-07-17 14:27:54 +0200

    supression des fichier et revus du code inutile

--- zen-massage-backend/src/prisma.ts
+++ zen-massage-backend/src/prisma.ts
@@ -1,7 +1,5 @@
-import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL as string })
 export const prisma = new PrismaClient({ adapter })
